d9134d9c3b066c2ee3efcf6e37bf3921d3f05598
Gourav Mallick <your-email@example.com>
2026-02-17 09:56:11 +0530

    fixed cardreader mode and add attandance clear btn

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.example.login"
         minSdk = 19
         targetSdk = 36
-        versionCode = 2
-        versionName = "2.1"
+        versionCode = 3
+        versionName = "2.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
